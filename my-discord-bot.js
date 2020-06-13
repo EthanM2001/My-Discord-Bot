41215d2d2b948104ede6179a30d0c84d5710b6ff
Ethan Moss <Ethanmoss2001@gmail.com>
2020-06-13 14:06:37 +0100

    Seeing the channels which my bot is currently being used one

--- my-discord-bot.js
+++ my-discord-bot.js
@@ -3,9 +3,16 @@ const client = new Discord.Client();
 const bot_token = require('./token')
 
 client.on('ready', () => {
-    console.log("Connected as " + client.user.tag)
+    console.log("Connected as " + client.user.tag);
     
     client.user.setActivity("in VSCode", { type: "PLAYING" });
+
+    client.guilds.cache.forEach((guild) => {
+        console.log(guild.name);
+        guild.channels.cache.forEach((channel) => {
+            console.log(`${channel.name}, ${channel.type}, ${channel.id}`);
+        })
+    })
 })
 
 client.login(bot_token);
